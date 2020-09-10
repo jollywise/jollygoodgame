03ba933a0adb1ae23eecf388a4134a80f3702e2b
Stephen Hamilton <stephenhamilton@mac.com>
2020-09-10 13:26:27 +0100

    Adds groups , displayField , toggles, dropdowns, and a stub implementation for when shortcuts are disabled
    Signed-off-by: Stephen Hamilton <stephenhamilton@mac.com>

--- src/shortcuts/Shortcuts.js
+++ src/shortcuts/Shortcuts.js
@@ -7,6 +7,8 @@ export class Shortcuts {
   constructor(game) {
     this.game = game;
     this.model = {};
+    this.groups = {};
+    this.defaultGroup = 'game';
     this.controls = [];
     this.isVisible = __WATCH__;
 
@@ -56,13 +58,58 @@ export class Shortcuts {
     this.gui.domElement.style.position = 'absolute';
     this.gui.domElement.style.top = 0;
     this.game.scale.parent.appendChild(this.gui.domElement);
+    this.addShortcutGroup({ title: 'Game', open: true });
     this.close();
     this.setVisible();
   }
 
-  addShortcut({ field, title, value = null }) {
+  addShortcutGroup({ id = this.defaultGroup, title, open = false }) {
+    const group = this.gui.addFolder(title);
+    this.groups[id] = group;
+    open && group.open();
+    return group;
+  }
+
+  addDisplayField(opts) {
+    const control = this.addShortcut(opts);
+    const el = control.domElement.getElementsByTagName('input')[0]; // make input read only
+    el && (el.readOnly = true);
+    return control;
+  }
+
+  addShortcut({ group = this.defaultGroup, field, title, value = null }) {
     this.model[field] = value;
-    this.controls.push(this.gui.add(this.model, field).name(title));
+    const control = this.groups[group].add(this.model, field).name(title);
+    this.controls.push(control);
+    return control;
+  }
+
+  addToggle({ group = this.defaultGroup, field, title, value = false, onChange = null }) {
+    this.model[field] = value;
+    const control = this.groups[group].add(this.model, field).name(title);
+    onChange && control.onChange(onChange);
+    this.controls.push(control);
+    return control;
+  }
+
+  /*
+   * this.shortcuts.addDropDown({ field: '', title: '', value: [array of data], onChange: (val) => { // do something with 'val' here } });
+   */
+  addDropDown({ group = this.defaultGroup, field, title, value = [], onChange = null }) {
+    this.model[field] = value;
+    const control = this.groups[group].add(this.model, field, value).name(title);
+    onChange && control.onChange(onChange);
+    this.controls.push(control);
+    return control;
+  }
+
+  /*
+   * Updates components : Use after changing a model value externally
+   */
+  updateDisplay() {
+    this.controls.forEach((item) => {
+      item.updateDisplay();
+    });
   }
 
   reset() {
@@ -70,6 +117,10 @@ export class Shortcuts {
       item.remove();
     });
     this.controls = [];
+    Object.keys(this.groups).forEach((id) => {
+      this.gui.removeFolder(this.groups[id]);
+    });
+    this.groups = {};
   }
 
   destroy() {
@@ -81,4 +132,17 @@ export class Shortcuts {
     this.trigger.destroy();
     this.trigger = null;
   }
+}
+
+export class ShortcutStub {
+  constructor() {
+    this.model = {};
+  }
+  reset() {}
+  updateDisplay() {}
+  addShortcutGroup() {}
+  addDisplayField() {}
+  addShortcut() {}
+  addToggle() {}
+  addDropDown() {}
 }
--- types/Shortcuts.d.ts
+++ types/Shortcuts.d.ts
@@ -1,5 +1,15 @@
 export class Shortcuts {
   constructor(game: Phaser.Game);
+  model: object;
+  groups: object;
+  controls: [];
+  reset();
+  updateDisplay();
+  addShortcutGroup();
+  addDisplayField();
+  addShortcut();
+  addToggle();
+  addDropDown();
 }
 
 export class ShortcutTriggers {
