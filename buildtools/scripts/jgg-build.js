dd000e3c98040243b742e6c825c6395d010e7007
puzzledmonkey <chris@oedo.net>
2021-03-11 17:59:58 +0900

    try to add bin support

---
+++ buildtools/scripts/jgg-build.js
@@ -0,0 +1,23 @@
+/**
+ * Build scripts
+ */
+const fs = require('fs');
+const path = require('path');
+const exec = require('child_process').exec;
+
+if (process.argv.length === 0) {
+  return;
+}
+const script = process.env[0].replace('/', '').replace('\\', '');
+if (!fs.existsSync(path.resolve(__dirname, script + '.js'))) {
+  console.log('Unknown build script ' + script);
+  return;
+}
+
+exec('node ' + path.resolve(__dirname, script + '.js'), function (error, stdout, stderr) {
+  console.log('stdout: ' + stdout);
+  console.log('stderr: ' + stderr);
+  if (error !== null) {
+    console.log('exec error: ' + error);
+  }
+});
