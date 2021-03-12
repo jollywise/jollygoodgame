d71cd728c7867f928e69ad90e91ba58014a98053
Paul Garrett <15093657+Jiminibob@users.noreply.github.com>
2021-03-12 22:06:33 +0000

    Document SceneControllerBase

--- src/controller/SceneControllerBase.js
+++ src/controller/SceneControllerBase.js
@@ -1,9 +1,35 @@
+/**
+ * @class SceneControllerBase
+ * @param {Phaser.Scenes.SceneManager} sceneManager The game scene manager
+ * @description A scene controller for managing scenes used in game.
+ * <br>
+ * This controller provides easy ability to add scenes, as well as control which scenes are active and trigger overlays.
+ */
 export class SceneControllerBase {
   constructor(sceneManager) {
     this.sceneManager = sceneManager;
     this.currentSceneKey = null;
     this.paused = false;
   }
+
+  /**
+   *
+   * @description Add a scene map to the controller. <br>The controller will parse each scene in the map and add them to phasers scene manager.
+   * @param {Object} sceneMap A scene map to add to the controller key/scene dictionary
+   * @example
+   *
+   * // state = imported Phaser.Scenes.Scene class
+   *
+   * const SCENE_MAP = [
+   *  { key: 'Welcome', state: Welcome },
+   *  { key: 'Complete', state: Complete },
+   *  { key: 'HowToPlay', state: HowToPlay },
+   *  { key: 'GamePlay', state: GamePlay },
+   * ];
+   *
+   * sceneController.addSceneMap(SCENE_MAP);
+   *
+   */
 
   addSceneMap(sceneMap) {
     sceneMap.map(({ key, state }) => {
@@ -17,13 +43,35 @@ export class SceneControllerBase {
     this.currentSceneKey = null;
   }
 
+  /**
+   * @returns {Phaser.Scenes.Scene}
+   * @description returns the current active scene
+   *
+   */
+
   getCurrentScene() {
     return this.sceneManager.getScene(this.currentSceneKey);
   }
 
+  /**
+   *
+   * @param {string} id scene key - should match a key provided in the {@link addSceneMap | Scene Map}
+   * @returns {Phaser.Scenes.Scene} the requested scene
+   * @description request a specific scene from the manager.
+   *
+   */
+
   getScene(id) {
     return this.sceneManager.getScene(id);
   }
+
+  /**
+   *
+   * @param {bool=} isActive return all scenes or active ones only
+   * @returns {Phaser.Scenes.Scene[]} an array of all scenes found
+   * @description Get all active scenes, if isActive is set to false, the it will return every scene added in the game
+   *
+   */
 
   getScenes(isActive = true) {
     return this.sceneManager.getScenes(isActive);
@@ -44,17 +92,34 @@ export class SceneControllerBase {
     }
   }
 
-  switchScene(sceneKey, { data } = {}) {
+  /**
+   *
+   * @param {string} sceneKey The key of the scene to start, should match a key provided in the Scene Map.
+   * @param {Object} data Data that will be passed through to the scenes init method whenn starting.
+   * @description Switch game scenes, this will stop the current active scene and start the requested scene. <br>The new scene will automatically be brought to the top of the scene display list.
+   *
+   */
+
+  switchScene(sceneKey, cdatafig) {
     // console.log('switchScene', sceneKey);
     if (this.currentSceneKey) {
       this.stopScene(this.currentSceneKey);
     }
     this.currentSceneKey = sceneKey;
-    this.startScene(this.currentSceneKey, { data });
+    this.startScene(this.currentSceneKey, data);
     if (this.paused) {
       this.pauseScene(this.currentSceneKey);
     }
   }
+
+  /**
+   *
+   * @param {string} scene The key of the scene to be started, should match a key in the Scene Map.
+   * @param {Object} data Data that will be passed through to the scenes init method when starting.
+   * @description Start a scene. Usually you would use switchScene instead how-ever this can be called directly to start a particular scene.<br>
+   * Calling this directly will not stop and current active scenes.
+   *
+   */
 
   startScene(scene, data) {
     this.sceneManager.start(scene, data);
@@ -62,19 +127,48 @@ export class SceneControllerBase {
     this.sceneManager.bringToTop(scene);
   }
 
+  /**
+   *
+   * @param {string} scene The key of the scene to be stopped, should match a key in the Scene Map.
+   * @description Will stop the requested scene from running, it is not destroyed and can be restarted again if desired.
+   *
+   */
+
   stopScene(scene) {
     this.sceneManager.stop(scene);
     this.sceneManager.getScene(scene).scene.setVisible(false);
   }
 
+  /**
+   *
+   * @param {string} scene The key of the scene to be paused, should match a key in the Scene Map.
+   * @description pause a desired scene
+   *
+   */
+
   pauseScene(scene) {
     this.sceneManager.pause(scene);
   }
+
+  /**
+   *
+   * @param {string} scene The key of the scene to be paused, should match a key in the Scene Map.
+   * @description resume a desired scene
+   *
+   */
 
   resumeScene(scene) {
     this.sceneManager.resume(scene);
     this.sceneManager.bringToTop(scene);
   }
+
+  /**
+   *
+   * @param {string} sceneKey The key of the scene to be obverlayed, should match a key in the Scene Map.
+   * @param {Object} data  Data that will be passed through to the scenes init method when starting. <br>Also accepts a callback for onOverlayCLosed which will be called when the overlay is closed of provided.
+   * @description Show a scene as an overlay. This will pause the current active scene and display the requested scene over the top
+   *
+   */
 
   showOverlayScene(sceneKey, { data, onOverlayClosed = null } = {}) {
     if (this.overlaySceneKey) this.removeOverlayScene();
@@ -86,6 +180,12 @@ export class SceneControllerBase {
       this.pauseScene(sceneKey);
     }
   }
+
+  /**
+   *
+   * @description Close the current overlay and resume the current active scene.
+   *
+   */
 
   removeOverlayScene() {
     if (this.overlaySceneKey) {
@@ -102,11 +202,23 @@ export class SceneControllerBase {
     }
   }
 
+  /**
+   *
+   *  @description quick pause current scene or overlay.
+   *
+   */
+
   pause() {
     this.paused = true;
     this.pauseScene('soundController');
     this.pauseScene(this.overlaySceneKey ? this.overlaySceneKey : this.currentSceneKey);
   }
+
+  /**
+   *
+   * @description quick resume current scxene or overlay.
+   *
+   */
 
   resume() {
     this.paused = false;
--- src/hud/HudEventMapBase.js
+++ src/hud/HudEventMapBase.js
@@ -1,11 +1,4 @@
 export class HudEventMapBase {
-  /**
-   *
-   * @param {Hud} hud The Hud in which to listen for Events
-   * @param {Object} controller The controller which has the methods to be called
-   * @param {Object} map Map of events and buttons to listen for, and what methods to call when triggered
-   *
-   */
   constructor(hud, controller, map = HudEventMapConfigBase) {
     this._hud = hud;
     this._map = map;
@@ -20,10 +13,6 @@ export class HudEventMapBase {
       this._controller[this._buttonMap[btnid]].call(this._controller);
     }
   }
-
-  /**
-   * enable Hud Events, this will react to Hud Actions
-   */
   enable() {
     for (const event in this._eventMap) {
       if (this._eventMap.hasOwnProperty(event)) {
@@ -33,9 +22,6 @@ export class HudEventMapBase {
     this._hud.events.on('BUTTON_SELECT', this._handleButtonSelect, this);
   }
 
-  /**
-   * disable Hud Events, this will no longer react to Hud Actions
-   */
   disable() {
     for (const event in this._eventMap) {
       if (this._eventMap.hasOwnProperty(event)) {
@@ -45,9 +31,6 @@ export class HudEventMapBase {
     this._hud.events.off('BUTTON_SELECT', this._handleButtonSelect, this);
   }
 
-  /**
-   * destroy this, disables and nullifies
-   */
   destroy() {
     this.disable();
 
