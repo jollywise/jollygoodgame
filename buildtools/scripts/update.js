cb1da7649477098dfbb47c66207327f2767b39e8
puzzledmonkey <chris@oedo.net>
2021-03-12 20:37:06 +0900

    add update script

---
+++ buildtools/scripts/update.js
@@ -0,0 +1,73 @@
+/**
+ * Update package.json
+ */
+
+const fs = require('fs');
+const path = require('path');
+const { cosmiconfigSync } = require('cosmiconfig');
+
+const cosmiconfig = cosmiconfigSync('jggUpdate').search();
+const config = cosmiconfig ? cosmiconfig.config || {} : {};
+
+const ROOT_DIRECTORY = config.rootDirectory || path.resolve('.');
+const PACKAGE_JSON = config.packageJson || path.resolve(ROOT_DIRECTORY, 'package.json');
+const NODE_MODULES = config.nodeModules || path.resolve(ROOT_DIRECTORY, 'node_modules/@jollywise');
+const PACKAGE_FRAGMENT = path.resolve(ROOT_DIRECTORY, 'package-fragment.json');
+
+const readPackage = (file) => {
+  if (!fs.existsSync(file)) {
+    return {};
+  }
+  try {
+    return JSON.parse(fs.readFileSync(file));
+  } catch (err) {
+    console.log('Error reading ' + file, err);
+  }
+};
+
+const writePackage = (file, data) => {
+  try {
+    fs.writeFileSync(file, JSON.stringify(data));
+  } catch (err) {
+    console.log('Error writing ' + file, err);
+  }
+};
+
+const getJGGModules = () => {
+  if (!fs.existsSync(NODE_MODULES)) {
+    return [];
+  }
+  const modules = [];
+  fs.readdirSync(NODE_MODULES).forEach((item) => {
+    if (!/^\..*/.test(item)) {
+      if (fs.lstatSync(path.join(NODE_MODULES, item)).isDirectory()) {
+        modules.push(item);
+      }
+    }
+  });
+  return modules;
+};
+
+const mergePackageData = (current, merge) => {
+  Object.keys(merge).forEach((key) => {
+    if (!current[key]) {
+      current[key] = merge[key];
+    } else if (typeof merge[key] === 'string') {
+      current[key] = merge[key];
+    } else {
+      Object.keys(merge[key]).forEach((key2) => {
+        current[key][key2] = merge[key][key2];
+      });
+    }
+  });
+};
+
+const current = readPackage(PACKAGE_FRAGMENT);
+getJGGModules().forEach((el) => {
+  const fragment = path.join(NODE_MODULES, `${el}/buildtools/package-fragment.json`);
+  if (fs.existsSync(fragment)) {
+    mergePackageData(current, readPackage(fragment));
+  }
+});
+mergePackageData(current, readPackage(PACKAGE_FRAGMENT));
+writePackage(PACKAGE_JSON, current);
